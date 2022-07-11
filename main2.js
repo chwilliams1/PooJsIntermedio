ae243ef18a5a408fa6a54f67bee47f84da278265
Charles Duarte <charlesduarte97@gmail.com>
2022-07-11 17:39:34 -0400

    Añadi main2, referente a clase 7

---
+++ main2.js
@@ -0,0 +1,25 @@
+obj1 = {
+    a:"a",
+    b:"b",
+    c:{d:"d",e:"e"},
+    editA(){
+        this.a ="AAAAAAAA"
+    }
+}
+
+const stringifiedComplexObj = JSON.stringify(obj1);
+
+const obj2 = JSON.parse(stringifiedComplexObj);
+
+
+
+
+/*
+for (prop in obj1){
+    obj2[prop] = obj1[prop];}
+
+const obj3 = Object.assign({},obj1);
+
+const obj4 = Object.create(obj1); */
+
+
